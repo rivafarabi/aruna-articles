a2a2cfed12d28f132c615f37fc59a65e6f1ab30c
rivafarabi <rivafarabi.babeheer@gmail.com>
2022-09-06 11:40:25 +0700

    remove shared_preference from service

--- lib/services/article_service.dart
+++ lib/services/article_service.dart
@@ -1,14 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:aruna_articles/models/article_post.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ArticleService {
-  late SharedPreferences prefs;
-
-  ArticleService() {
-    SharedPreferences.getInstance().then((value) => prefs = value);
-  }
-
   Future<List<ArticlePost>> fetchArticles() async {
     try {
       var response = await Dio().get(
